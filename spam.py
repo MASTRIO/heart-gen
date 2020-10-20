52b341b5258a64936672583cdd6177864f4521d5
MASTRIO <56378891+MASTRIO@users.noreply.github.com>
2020-10-20 22:57:57 +1000

    spam.py

---
+++ spam.py
@@ -0,0 +1,16 @@
+# Import
+import keyboard
+import time
+
+# Variables
+timesSpammed = 0
+
+# Epic Gamer Code
+time.sleep(10)
+
+while True:
+    time.sleep(1)
+    keyboard.press_and_release('h+e+h+e')
+    keyboard.press_and_release('enter')
+    timesSpammed = timesSpammed + 1
+    print("Times Spammed:", timesSpammed)
