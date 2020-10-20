980ae93f764f46df8e32a43dbc6542fc5aef5fb7
MASTRIO <56378891+MASTRIO@users.noreply.github.com>
2020-10-20 23:03:47 +1000

    oofs

--- spam.py
+++ spam.py
@@ -9,7 +9,7 @@ timesSpammed = 0
 time.sleep(10)
 
 while True:
-    time.sleep(1)
+    time.sleep(2)
     keyboard.press_and_release('h+e+h+e')
     keyboard.press_and_release('enter')
     timesSpammed = timesSpammed + 1
